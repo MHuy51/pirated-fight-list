34d937dd00d23cb677dd2611e66ae6e29d2b8591
MHuy51 <154833379+MHuy51@users.noreply.github.com>
2024-01-12 16:50:44 -0500

    Delete triviaGame directory

--- triviaGame/src/game/BonusRound.java
+++
@@ -1,9 +0,0 @@
-package game;
-
-public class BonusRound extends Round{
-	private static final long serialVersionUID = 6217780994996022646L;
-
-	public void displayMessage() {
-		System.out.println("BONUS ROUND!!! DOUBLE POINTS!!!");
-	}
-}
